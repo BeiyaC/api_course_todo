6ae75aaa15768f59c13950f8fee00d059b3b2794
BeiyaC <anis.grigah@le-campus-numerique.fr>
2024-11-19 14:44:55 +0100

    change host env

--- core/db.js
+++ core/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    'host': '0.0.0.0',
+    'host': process.env.DB_HOST,
     'user': process.env.DB_USER,
     'password': process.env.DB_PASSWORD,
     'database': process.env.DB_NAME
